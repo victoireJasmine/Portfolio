029a80824d15e346d64cbf15f8d61e7df428ec48
Vivie jas OND <viejasmine0@gmail.com>
2022-03-03 12:05:27 +0100

    body & footer

---
+++ src/assets/js/data.js
@@ -0,0 +1,33 @@
+const items =[
+    {id:1, type:"page web", title:"calculatrice", img:"realisation.jpg", link:"calculatrice.net"},
+    {id:2, type:"page web", title:"calculatrice", img:"realisation.jpg", link:"calculatrice.net"},
+   
+    
+
+]
+
+
+const $displayItems = document.querySelector(".rea--items");
+
+if ($displayItems) {
+    items.forEach(element => {
+        $displayItems.innerHTML += `
+       
+        ${__dirname}
+            <a href="${element.link}" class="rea--item" target="_blank">
+                <div class="rea--illustration">
+                    <img src="${require('url:~/src/assets/img/'+element.img)}" alt=""/>
+                    <div class="rea--bg"></div>
+                </div>
+               <div class="rea--desc">
+                   <p> ${element.type}</p>
+                   <h2>${element.title}</h2>
+               </div>
+            </a>
+            
+        `
+        
+        
+    });
+    
+}
